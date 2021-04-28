6d00b2244ee166707b83d5a118ba2b925bc4f0c6
MomentumV <MomentumV@users.noreply.github.com>
2021-04-28 09:38:01 -0400

    Initial Commit

---
+++ config.js
@@ -0,0 +1,136 @@
+var CONFIG = {
+  "maps": {
+    "Caves": {
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1619556996,
+        1619556996,
+        1619556996,
+        1619556996
+      ],
+      "maxZoom": 6,
+      "name": "Caves",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 16,
+      "tileSetGroup": "thirdlife_isometric_t1",
+      "tileSize": [
+        512,
+        512
+      ],
+      "world": "thirdlife",
+      "worldName": "thirdlife",
+      "worldSeaLevel": 64
+    },
+    "Isometric": {
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1619556996,
+        1619556996,
+        1619556996,
+        1619556996
+      ],
+      "maxZoom": 6,
+      "name": "Isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 16,
+      "tileSetGroup": "thirdlife_isometric_t1",
+      "tileSize": [
+        512,
+        512
+      ],
+      "world": "thirdlife",
+      "worldName": "thirdlife",
+      "worldSeaLevel": 64
+    },
+    "Top View": {
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1619556996,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "Top View",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 16,
+      "tileSetGroup": "thirdlife_topdown_t1",
+      "tileSize": [
+        256,
+        256
+      ],
+      "world": "thirdlife",
+      "worldName": "thirdlife",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "Top View",
+    "Isometric",
+    "Caves"
+  ],
+  "tileSetGroups": {
+    "thirdlife_isometric_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          -1,
+          3
+        ],
+        [
+          15,
+          -4
+        ],
+        [
+          31,
+          3
+        ],
+        [
+          15,
+          11
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "thirdlife_topdown_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
