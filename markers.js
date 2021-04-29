f0788b2ee3c7ecf5d36021eb469cee727409147c
MomentumV <MomentumV@users.noreply.github.com>
2021-04-28 23:34:06 -0400

    Input spawn start locations

--- markers.js
+++ markers.js
@@ -1,7 +1,7 @@
 // Add your own markers to this file.
 
 var MAPCRAFTER_MARKERS = [
-	// Session One
+	// Session One Events
 	{
 		// id of the marker group, without spaces/other special chars
 		"id" : "session1",
@@ -12,18 +12,83 @@ var MAPCRAFTER_MARKERS = [
 		// size of that icon
 		"iconSize" : [24, 24],
 		// whether this marker group is shown by default (optional)
+		"showDefault" : false,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"thirdlife" : [
+
+				{"pos" : [0, 255, 93], "title" : "Impulse finds pants and bread"},
+				{"pos" : [-108, 230, 64], "title" : "Refugee Party House"}
+				{"pos" : [80, 17, 64], "title" : "Tango meets Smallbeans"}
+				{"pos" : [79, 87, 62], "title" : "Tango on fire from zombie and get shield"}
+				{"pos" : [130, 108, 68], "title" : "Etho gets shield from Smallbeans and Tango"}
+				{"pos" : [272, 294, 69], "title" : "Tango and Smallbeans meet Grian and BigB"}
+				{"pos" : [190, 345, 64], "title" : "worthers originals"},
+				{"pos" : [220, 300, 64], "title" : "The offering"},
+				{"pos" : [275, 148, 64], "title" : "Ruined Portal"}
+				
+				
+			],
+		},
+	},	
+	//bases
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "bases",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Bases",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "home.png",
+		// size of that icon
+		"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"thirdlife" : [
+				{"pos" : [192, 78, 64], "title" : "Renchanting"},
+				{"pos" : [-131, 333, 64], "title" : "Valley People"},				
+				{"pos" : [-296, -97, 64], "title" : "Pride Rock/Sand Castle"}
+				
+				
+			],
+		},
+	},	
+		//spawns
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "spawn locations",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Starting points",
+		// icon of the markers belonging to that group (optional)
+		//"icon" : "home.png",
+		// size of that icon
+		//"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
 		"showDefault" : true,
 		// markers of this marker group...
 		"markers" : {
 			// ...in the world "world"
 			"thirdlife" : [
 
-				{"pos" : [190, 345, 64], "title" : "worthers originals"},
-				{"pos" : [220, 300, 64], "title" : "The offering"},
-				{"pos" : [-131, 333, 64], "title" : "Valley People"},
-				{"pos" : [275, 148, 64], "title" : "Ruined Portal"}
-				
+				{"pos" : [10, 267, 94], "title" : "Bdubs", "icon":"Bdubs.png"},
+				{"pos" : [8, 263, 96], "title" : "Cleo", "icon":"Cleo.png"},
+				{"pos" : [70, 255, 74], "title" : "Impulse", "icon":"Impulse.png"},
+				{"pos" : [65, -4, 74], "title" : "Smallbeans", "icon":"Smallbeans.png"},
+				{"pos" : [113, 150, 80], "title" : "Etho", "icon":"Etho.png"},
+				{"pos" : [-24, -110, 69], "title" : "Scott", "icon":"Smajor1995.png"},
+				{"pos" : [180, 331, 78], "title" : "BigB", "icon":"BigB.png"},
+				{"pos" : [202, 344, 75], "title" : "LittleWood", "icon":"LittleWood.png"},
+				{"pos" : [259, 157, 63], "title" : "Grian", "icon":"Grian.png"},
+				{"pos" : [36, 112, 63], "title" : "ReNDoG", "icon":"ReNDoG.png"},
+				{"pos" : [77, 34, 67], "title" : "Tango", "icon":"Tango.png"},
+				{"pos" : [-90, 327, 87], "title" : "Scar", "icon":"Scar.png"},
+				{"pos" : [-294, 140, 62], "title" : "Solidarity", "icon":"Solidarity.png"},
+				{"pos" : [-284, -7, 66], "title" : "Skizz", "icon":"Skizz.png"},
 			],
 		},
 	},	
+
 ];
